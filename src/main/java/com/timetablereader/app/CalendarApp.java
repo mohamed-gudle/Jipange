320abb335b6481c56bed4d2b1bd60be0df85d89f
mohamed-gudle <58801991+mohamed-gudle@users.noreply.github.com>
2020-11-24 15:35:51 +0300

    this commit connects the application to mongodb and writes the school day data to a collection in the database

--- src/main/java/com/timetablereader/app/CalendarApp.java
+++ src/main/java/com/timetablereader/app/CalendarApp.java
@@ -21,11 +21,18 @@ import com.calendarfx.model.Calendar.Style;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
 import com.calendarfx.view.CalendarView;
-import javafx.application.Application;
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.bson.Document;
+
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -34,13 +41,17 @@ import java.util.ArrayList;
 public class CalendarApp{
 
     private static ArrayList<SchoolDay> schoolDays;
+    private MongoConnection connection=new MongoConnection();
+    private MongoDatabase init = connection.init();
+    private MongoCollection schoolDayTable = connection.getCollection("day");
+
     public void setSchoolDays(ArrayList<SchoolDay> schoolDays) {
         CalendarApp.schoolDays =schoolDays;
 
     }
 
 
-    public void init(Stage primaryStage) throws Exception {
+    public void  init(Stage primaryStage) throws Exception {
         CalendarView calendarView = new CalendarView();
 
         Calendar classes = new Calendar(" Classes");
@@ -147,15 +158,7 @@ public class CalendarApp{
                     entry.setTitle(hour.getLesson().getName() + " :" + hour.getLesson().getLecturerName());
                     entry.setId(lesson.getCode());
                     entry.changeStartDate(localDate);
-
-
-
-
-
-
-
-
-
+                    entry.changeEndDate(localDate);
 
                     switch (i)
                     {
@@ -221,7 +224,17 @@ public class CalendarApp{
             localDate=monday;
             j++;
 
+
+            Gson gson = new Gson();
+            String json = gson.toJson(day);
+            // Parse to bson document and insert
+            Document doc = Document.parse(json);
+
+            schoolDayTable.insertOne(doc);
+
         }
+
+
 
 
 
