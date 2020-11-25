98a7bedcc123716cb1ce4051291817ebfc2b802e
Mike Mwangi <mikemwangi0551@gmail.com>
2020-11-25 16:03:14 +0300

    File upload page done

--- src/main/java/com/timetablereader/app/Controllers/FileScreen.java
+++ src/main/java/com/timetablereader/app/Controllers/FileScreen.java
@@ -1,4 +1,108 @@
 package com.timetablereader.app.Controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.timetablereader.app.CalendarView;
+import com.timetablereader.app.ExcelReader;
+import com.timetablereader.app.data.SchoolDay;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class FileScreen {
+
+
+    private static int currentScreen = 1;
+
+    @FXML
+    private VBox mainContent;
+
+    @FXML
+    private JFXButton fileSelector;
+
+    @FXML
+    private JFXButton continueBtn;
+
+    private TextField tableNumber, textField;
+    private File selectedFile;
+
+
+    ExcelReader excelReader = new ExcelReader();
+
+
+    private CalendarView calendarView=new CalendarView();
+    private static Stage stage1;
+
+
+    public static void setStage(Stage stage) {
+        stage1 = stage;
+    }
+
+    @FXML
+    void fileSelector(ActionEvent event) {
+        selectedFile = new FileChooser().showOpenDialog(new Stage());
+
+    }
+
+    @FXML
+    void nextScreen(ActionEvent event) {
+        changeScreen(currentScreen);
+
+    }
+
+    public void changeScreen(int screenNumber) {
+        switch (screenNumber) {
+            case 1: {
+                try {
+
+                    excelReader.setWorkbook(selectedFile);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println(e.getMessage());
+
+                }
+                mainContent.getChildren().clear();
+                if (excelReader.getNumberOfSheets() > 1) {
+                    textField = new TextField("sheetNumber");
+                    mainContent.getChildren().add(textField);
+
+                }
+                currentScreen++;
+                break;
+            }
+            case 2: {
+                excelReader.setSheet(0);
+                mainContent.getChildren().clear();
+                if (excelReader.getNumberOfTables() > 1) {
+                    tableNumber = new TextField("Table number");
+                    mainContent.getChildren().add(tableNumber);
+                }
+                currentScreen++;
+                break;
+            }
+            case 3: {
+                int region = (Integer.parseInt(tableNumber.getText()) - 1) * 10 + 3;
+                excelReader.setRegion(23);
+                ArrayList<SchoolDay> schoolDays = excelReader.readFile();
+                try {
+                    calendarView.setSchoolDays(schoolDays);
+                    calendarView.init(stage1);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    System.out.println(e.getMessage());
+                }
+                currentScreen++;
+                break;
+
+            }
+
+        }
+    }
 }
--- src/main/java/com/timetablereader/app/JipangeApplication.java
+++ src/main/java/com/timetablereader/app/JipangeApplication.java
@@ -1,11 +1,13 @@
 package com.timetablereader.app;
 
+import com.timetablereader.app.Controllers.FileScreen;
 import com.timetablereader.app.data.SchoolDay;
 import com.timetablereader.app.utils.JipangeView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
@@ -22,62 +24,64 @@ public class JipangeApplication extends Application {
     @Override
     public void start(final Stage primaryStage) throws IOException {
 
-   /*     JipangeView root = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
+        FXMLLoader fxmlLoader=new FXMLLoader();
+        AnchorPane root =fxmlLoader.load(getClass().getClassLoader().getResource("FileScreen.fxml"));
 
+        FileScreen.setStage(primaryStage);
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
 
-    */
-        primaryStage.setTitle("JavaFX App");
 
-        FileChooser fileChooser = new FileChooser();
-
-
-      Button   button = new Button("Select File");
-        button.setOnAction(e -> {
-            final File selectedFile= fileChooser.showOpenDialog(primaryStage);
-            try {
-                ExcelReader excelReader=new ExcelReader();
-
-                excelReader.setWorkbook(selectedFile);
-
-                excelReader.setSheet(0);
-               if(excelReader.getNumberOfSheets()>1){
-                   //display getSheetPage;
-                   //get number pass it to excelReader.setSheet(0);
-                   //diplay getTable;
-
-               }
-               if(excelReader.getNumberOfTables()>1){
-                   //display getSheetPage;
-                   //get number pass it to excelReader.setSheet(0);
-
-                   //diplay getTable;
-
-               }
-
-                excelReader.setRegion(23);
-                ArrayList<SchoolDay> schoolDays = excelReader.readFile();
-
-                CalendarView calendarView=new CalendarView();
-                calendarView.setSchoolDays(schoolDays);
-                calendarView.init(primaryStage);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-
-
-        VBox vBox = new VBox(button);
-        Scene scene = new Scene(vBox, 960, 600);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
-    }
+//        primaryStage.setTitle("JavaFX App");
+//
+//        FileChooser fileChooser = new FileChooser();
+//
+//
+//      Button   button = new Button("Select File");
+//        button.setOnAction(e -> {
+//            final File selectedFile= fileChooser.showOpenDialog(primaryStage);
+//            try {
+//                ExcelReader excelReader=new ExcelReader();
+//
+//                excelReader.setWorkbook(selectedFile);
+//
+//                excelReader.setSheet(0);
+////               if(excelReader.getNumberOfSheets()>1){
+////                   //display getSheetPage;
+////                   //get number pass it to excelReader.setSheet(0);
+////                   //diplay getTable;
+////
+////               }
+////               if(excelReader.getNumberOfTables()>1){
+////                   //display getSheetPage;
+////                   //get number pass it to excelReader.setSheet(0);
+////
+////                   //diplay getTable;
+////
+////               }
+//
+//                excelReader.setRegion(23);
+//                ArrayList<SchoolDay> schoolDays = excelReader.readFile();
+//
+//                CalendarView calendarView=new CalendarView();
+//                calendarView.setSchoolDays(schoolDays);
+//                calendarView.init(primaryStage);
+//            } catch (Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        });
+//
+//
+//        VBox vBox = new VBox(button);
+//        Scene scene = new Scene(vBox, 960, 600);
+//
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//
+//
+   }
 
  public static void main(String args[]){
         launch(args);
